c4eca542e8c2ec65deb8e03dcf011655c92e60ee
Rasmus Einarsson <mr@rasmuseinarsson.se>
2022-02-13 19:38:08 +0100

    Start recording all kinds of settings

---
+++ conf/bash-settings.sh
@@ -0,0 +1,8 @@
+# Base16 Shell
+# See https://github.com/chriskempson/base16-shell.git
+BASE16_SHELL="$HOME/conf/base16-shell/"
+[ -n "$PS1" ] && \
+    [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+        eval "$("$BASE16_SHELL/profile_helper.sh")"
+
+base16_gruvbox-light-hard
